baac045ff4b1d9b7757cc24e48e1ea1ecbcc7163
Joshua Germon <joshgermon@gmail.com>
2023-04-20 09:36:42 +1000

    Seperated module and added clap

--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,26 @@
-use std::env;
+mod transactions;
+
 use dotenv::dotenv;
-use reqwest::{self, header::AUTHORIZATION};
-use serde::Deserialize;
+use transactions::{get_transactions, parse_transactions};
+use clap::Parser;
 
-#[derive(Debug, Deserialize)]
-pub struct TransactionList {
-    data: Vec<Datum>,
-    links: TransactionListLinks,
-}
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
 
-#[derive(Debug, Deserialize)]
-pub struct Datum {
-    #[serde(rename = "type")]
-    datum_type: String,
-    id: String,
-    attributes: Attributes,
-    relationships: Relationships,
-    links: DatumLinks,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Attributes {
-    status: Option<String>,
-    #[serde(rename = "rawText")]
-    raw_text: Option<String>,
-    description: Option<String>,
-    message: Option<String>,
-    #[serde(rename = "isCategorizable")]
-    is_categorizable: Option<bool>,
-    #[serde(rename = "holdInfo")]
-    hold_info: Option<serde_json::Value>,
-    #[serde(rename = "roundUp")]
-    round_up: Option<serde_json::Value>,
-    cashback: Option<serde_json::Value>,
-    amount: Amount,
-    #[serde(rename = "foreignAmount")]
-    foreign_amount: Option<serde_json::Value>,
-    #[serde(rename = "cardPurchaseMethod")]
-    card_purchase_method: Option<serde_json::Value>,
-    #[serde(rename = "settledAt")]
-    settled_at: Option<String>,
-    #[serde(rename = "createdAt")]
-    created_at: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Amount {
-    #[serde(rename = "currencyCode")]
-    currency_code: Option<String>,
-    value: Option<String>,
-    #[serde(rename = "valueInBaseUnits")]
-    value_in_base_units: Option<i64>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct DatumLinks {
-    #[serde(rename = "self")]
-    links_self: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Relationships {
-    account: Option<Account>,
-    #[serde(rename = "transferAccount")]
-    transfer_account: Option<ParentCategory>,
-    category: Option<Category>,
-    #[serde(rename = "parentCategory")]
-    parent_category: Option<ParentCategory>,
-    tags: Option<Category>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Account {
-    data: Option<Data>,
-    links: Option<AccountLinks>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Data {
-    #[serde(rename = "type")]
-    data_type: Option<String>,
-    id: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct AccountLinks {
-    related: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Category {
-    data: Option<Vec<Option<serde_json::Value>>>,
-    links: Option<DatumLinks>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct ParentCategory {
-    data: Option<serde_json::Value>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct TransactionListLinks {
-    prev: Option<serde_json::Value>,
-    next: Option<String>,
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
 }
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+    let args = Args::parse();
     let transactions = get_transactions().await;
     parse_transactions(transactions.unwrap());
 }
 
-async fn get_transactions() -> Result<TransactionList, String> {
-    let client = reqwest::Client::new();
-    let transaction_url = "https://api.up.com.au/api/v1/transactions";
-    let response = client.get(transaction_url)
-                        .header(AUTHORIZATION, format!("Bearer {}", env::var("UP_API_TOKEN").unwrap()))
-                        .query(&[("page[size]", "50")])
-                        .send().await.unwrap();
-
-    match response.status() {
-        reqwest::StatusCode::OK => {
-            let transaction_list = response.json::<TransactionList>().await.map_err(|e| format!("JSON deserialization error: {}", e))?;
-            Ok(transaction_list)
-        }
-        reqwest::StatusCode::UNAUTHORIZED => {
-            Err(String::from("Authentication error"))
-        }
-        _ => {
-            Err(format!("Unknown error: {}", response.status()))
-        }
-    }
-}
-
-fn parse_transactions(transactions: TransactionList) -> Result<(), String> {
-    let list = transactions.data;
-    let income: Vec<Datum> = list.into_iter().filter(|t| {
-            if let Some(value) = t.attributes.amount.value_in_base_units {
-                return value > 0;
-            }
-            false
-        }
-    ).collect();
-    let names_only: Vec<String>= income.into_iter().filter_map(|t| t.attributes.raw_text ).collect();
-    println!("{:?}", names_only);
-    Ok(())
-}
---
+++ src/transactions.rs
@@ -0,0 +1,162 @@
+use std::env;
+use serde::{Serialize, Deserialize};
+use reqwest::{self, header::AUTHORIZATION};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TransactionList {
+    data: Vec<Datum>,
+    links: TransactionListLinks,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Datum {
+    #[serde(rename = "type")]
+    datum_type: DatumType,
+    id: String,
+    attributes: Attributes,
+    relationships: Relationships,
+    links: DatumLinks,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Attributes {
+    status: Status,
+    #[serde(rename = "rawText")]
+    raw_text: Option<String>,
+    description: String,
+    message: String,
+    #[serde(rename = "isCategorizable")]
+    is_categorizable: bool,
+    #[serde(rename = "holdInfo")]
+    hold_info: Option<serde_json::Value>,
+    #[serde(rename = "roundUp")]
+    round_up: Option<serde_json::Value>,
+    cashback: Option<serde_json::Value>,
+    amount: Amount,
+    #[serde(rename = "foreignAmount")]
+    foreign_amount: Option<serde_json::Value>,
+    #[serde(rename = "cardPurchaseMethod")]
+    card_purchase_method: Option<serde_json::Value>,
+    #[serde(rename = "settledAt")]
+    settled_at: String,
+    #[serde(rename = "createdAt")]
+    created_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Amount {
+    #[serde(rename = "currencyCode")]
+    currency_code: CurrencyCode,
+    value: String,
+    #[serde(rename = "valueInBaseUnits")]
+    value_in_base_units: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DatumLinks {
+    #[serde(rename = "self")]
+    links_self: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Relationships {
+    account: Account,
+    #[serde(rename = "transferAccount")]
+    transfer_account: Account,
+    category: Category,
+    #[serde(rename = "parentCategory")]
+    parent_category: ParentCategory,
+    tags: Category,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Account {
+    data: Option<Data>,
+    links: Option<AccountLinks>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Data {
+    #[serde(rename = "type")]
+    data_type: DataType,
+    id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AccountLinks {
+    related: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Category {
+    data: Option<Vec<Option<serde_json::Value>>>,
+    links: Option<DatumLinks>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ParentCategory {
+    data: Option<serde_json::Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TransactionListLinks {
+    prev: Option<serde_json::Value>,
+    next: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum CurrencyCode {
+    #[serde(rename = "AUD")]
+    Aud,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Status {
+    #[serde(rename = "SETTLED")]
+    Settled,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum DatumType {
+    #[serde(rename = "transactions")]
+    Transactions,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum DataType {
+    #[serde(rename = "accounts")]
+    Accounts,
+}
+
+pub async fn get_transactions() -> Result<TransactionList, String> {
+    let client = reqwest::Client::new();
+    let transaction_url = "https://api.up.com.au/api/v1/transactions";
+    let response = client.get(transaction_url)
+                        .header(AUTHORIZATION, format!("Bearer {}", env::var("UP_API_TOKEN").unwrap()))
+                        .query(&[("page[size]", "50")])
+                        .send().await.unwrap();
+
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            let transaction_list = response.json::<TransactionList>().await.map_err(|e| format!("JSON deserialization error: {}", e))?;
+            Ok(transaction_list)
+        }
+        reqwest::StatusCode::UNAUTHORIZED => {
+            Err(String::from("Authentication error"))
+        }
+        _ => {
+            Err(format!("Unknown error: {}", response.status()))
+        }
+    }
+}
+
+pub fn parse_transactions(transactions: TransactionList) -> Result<(), String> {
+    let list = transactions.data;
+    let income: Vec<Datum> = list.into_iter().filter(|t| {
+            t.attributes.amount.value_in_base_units > 0
+        }
+    ).collect();
+    let names_only: Vec<String>= income.into_iter().filter_map(|t| t.attributes.raw_text ).collect();
+    println!("{:?}", names_only);
+    Ok(())
+}
